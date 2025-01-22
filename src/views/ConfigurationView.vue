008822a1623b794da3c11198f8180ac98ab1a200
Antoine Chabanel <antoinebaptistemozart2@gmail.com>
2025-01-22 18:02:01 +0100

    finished api integration

--- src/views/ConfigurationView.vue
+++ src/views/ConfigurationView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { serverAPI } from '@/composables/API';
 import type { ListElement, TextElement } from '@vueform/vueform';
 import { ref, useTemplateRef, watch, type Ref } from 'vue';
 
@@ -8,20 +9,29 @@ const nbPlayersRed : Ref<TextElement|null> = ref(null);
 const bluePlayerList : Ref<ListElement|null> = ref(null);
 const redPlayerList : Ref<ListElement|null>  = ref(null);
 
-const updateBluePlayerList = () => {
-  if (!bluePlayerList.value) return;
+const maxPlayers : number = 12; //TODO this value must be asked to the API
 
-  while (bluePlayerList.value?.size ?? 1 < nbPlayersBlue?.value?.value ?? 0)
-    bluePlayerList.value?.add({});
-  while (bluePlayerList.value?.size ?? 0 > nbPlayersBlue?.value?.value ?? 1)
+const updatePlayerList = (playerList : Ref<ListElement|null>, nbPlayers : Ref<TextElement|null>) => {
+  if (!playerList.value || !nbPlayers?.value) return;
+  else if (!(nbPlayers?.value ?? undefined))
+    return;
+
+  while (playerList.value?.length < parseInt(nbPlayers?.value?.toString() ?? '0')
+         &&  playerList.value?.length <= maxPlayers/2-1)
+    playerList.value?.add({});
+  while (playerList.value?.length > parseInt(nbPlayers?.value?.toString() ?? '0'))
   {
-    let lastIndex : number = parseInt(bluePlayerList.value?.size ?? '');
+    let lastIndex : number = playerList.value?.length - 1;
     if (!lastIndex) return;
-    bluePlayerList.value?.remove(lastIndex);
+    playerList.value?.remove(lastIndex);
   }
 };
-const updateRedPlayerList = () => {
 
+const updateBluePlayerList = () => {
+  updatePlayerList(bluePlayerList, nbPlayersBlue);
+};
+const updateRedPlayerList = () => {
+  updatePlayerList(redPlayerList, nbPlayersRed);
 };
 
 watch(nbPlayersBlue, updateBluePlayerList);
@@ -40,40 +50,70 @@ const onNbPlayerRedChange = (newValue: any) => {
 
 <template>
   <main>
-    <Vueform validate-on="change">
-      <TextElement name="nameBlue" label="Nom de l'équipe bleue" placeholder="Équipe bleue" />
-      <TextElement name="nameRed" label="Nom de l'équipe rouge" placeholder="Équipe rouge" />
-      <TextElement
-        name="nbPlayersBlue"
-        ref="nbPlayersBlue"
-        input-type="number"
-        :rules="['required','between:1,6','integer']"
-        :messages="{ between: 'Valeur entrée incorrecte.' }"
-        label="Nombre de joueurs bleus"
-        placeholder="6 max"
-        @change="onNbPlayerBlueChange"
+    <Vueform
+    validate-on="change"
+    class="container mt-4"
+    endpoint="/salut" method="post">
+
+      <StaticElement
+      name="titleBlue"
+      content="Équipe bleue"
+      description="Configurez les détails de l'équipe bleue"
+      tag="h2"
+      top="2"
       />
       <TextElement
-        name="nbPlayersRed"
-        ref="nbPlayersRed"
-        input-type="number"
-        :rules="['required','between:1,6','integer']"
-        :messages="{ between: 'Valeur entrée incorrecte.' }"
-        label="Nombre de joueurs rouges"
-        placeholder="6 max"
-        @change="onNbPlayerRedChange"
+      name="nameBlue"
+      label="Nom de l'équipe bleue"
+      placeholder="Équipe bleue"
+      rules="required"
+      />
+      <TextElement
+      name="nbPlayersBlue"
+      ref="nbPlayersBlue"
+      required="true"
+      input-type="number"
+      :rules="['required','between:1,'+maxPlayers/2,'integer']"
+      :messages="{ between: 'Valeur entrée incorrecte.' }"
+      label="Nombre de joueurs bleus"
+      placeholder="6 max"
+      @change="onNbPlayerBlueChange"
+      />
+      <ListElement
+      ref="bluePlayerList"
+      :disabled="true"
+      name="bluePlayerList"
+      :sort="true"
+      :object="{
+        schema: {
+          name: { type: 'text', placeholder: 'Pseudo du joueur', rules: 'required' },
+        }
+      }"
       />
 
-      <ListElement
-        ref="bluePlayerList"
-        :disabled="true"
-        name="bluePlayerList"
-        :sort="true"
-        :object="{
-          schema: {
-            name: { type: 'text'},
-          }
-        }"
+      <StaticElement
+      name="titleRed"
+      content="Équipe rouge"
+      description="Configurez les détails de l'équipe rouge"
+      tag="h2"
+      top="2"
+      />
+      <TextElement
+      name="nameRed"
+      label="Nom de l'équipe rouge"
+      placeholder="Équipe rouge"
+      rules="required"
+      />
+      <TextElement
+      name="nbPlayersRed"
+      ref="nbPlayersRed"
+      required="true"
+      input-type="number"
+      :rules="['required','between:1,'+maxPlayers/2,'integer']"
+      :messages="{ between: 'Valeur entrée incorrecte.' }"
+      label="Nombre de joueurs rouges"
+      placeholder="6 max"
+      @change="onNbPlayerRedChange"
       />
       <ListElement
         ref="redPlayerList"
@@ -82,10 +122,19 @@ const onNbPlayerRedChange = (newValue: any) => {
         :sort="true"
         :object="{
           schema: {
-            name: { type: 'text' },
+            name: { type: 'text', placeholder: 'Pseudo du joueur', rules: 'required' },
           }
         }"
       />
+
+      <ButtonElement
+      name="confirm"
+      :submits="true"
+      button-label="Valider les équipes"
+      :full="true"
+      size="lg"
+      />
+
     </Vueform>
   </main>
 </template>
